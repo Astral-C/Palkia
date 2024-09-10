f063ec3ba45e90e130356d420f1e34fe8d1c6f96
Astral-C <astral-c@users.noreply.github.com>
2024-09-10 11:29:34 -0400

    little updates

--- test/main.cpp
+++ test/main.cpp
@@ -8,28 +8,21 @@ int main(){
 	Tests tests;
 
 	
-	/*tests.registerTest("Get file from rom", [](){
+	tests.registerTest("Get file from rom", [](){
 
 		Palkia::NitroRom Platinum(std::filesystem::path("platinum.nds"));
 		NitroFile* zone_event_narc = Platinum.getFileByPath("fielddata/eventdata/zone_event.narc");
-		
 
 		if(zone_event_narc == nullptr){
 			std::cout << "Didn't find file :(" << std::endl;
 			return false;
 		} else {
+			
+			bStream::CMemoryStream zone_event_stream(zone_event_narc->data, zone_event_narc->size, bStream::Endianess::Little, bStream::OpenMode::In);
+			NitroArchive archive(zone_event_stream, true);
+			std::cout << ((uint32_t*)archive.getFileByIndex(0)->data)[0] << std::endl;
 			return true;
 		}
-	});*/
-
-	tests.registerTest("Get file from narc", [](){
-
-		bStream::CFileStream narc_file("arc0.narc", bStream::Endianess::Little, bStream::OpenMode::In);
-		NitroArchive archive(narc_file);
-
-		archive.dump();
-
-		return true;
 	});
 
 	tests.runTests();
